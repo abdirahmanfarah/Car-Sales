11526705ac7027500c6162bbfcbdab80e76322a4
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-10-09 13:55:23 -0700

    project setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import  carReducer  from './reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carReducer);
+console.log(store);
+
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider><App /> </Provider>, rootElement);
