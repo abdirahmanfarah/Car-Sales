5ccc402f277f1aaf452007d54869d196f5cd0206
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-10-09 16:12:34 -0700

    need to add action and event listeners

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import  carReducer  from './reducers'
+import  rootReducer  from './reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore(carReducer);
+const store = createStore(rootReducer);
 console.log(store);
 
 
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Provider><App /> </Provider>, rootElement);
+ReactDOM.render(<Provider store={store}>
+  <App /> 
+  </Provider>, 
+  rootElement);
